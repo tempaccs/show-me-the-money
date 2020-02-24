a2c1e42ba1edb9c6761bf36b5ded10030e895ac1
MS <belvostuff@protonmail.com>
2020-02-24 18:25:08 +0100

    Add redoc

--- showmethemoney/urls.py
+++ showmethemoney/urls.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework import routers, permissions
 from transactions.views import TransactionViewSet
 from customers.views import CustomerViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = routers.DefaultRouter()
 router.register(r'transactions', TransactionViewSet)
 router.register(r'customers', CustomerViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
